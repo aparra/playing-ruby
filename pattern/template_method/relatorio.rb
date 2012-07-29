94c915e66eda09810b3bf6d783f5be2f5d9bb81d
Anderson Parra <ander.pp@gmail.com>
2012-07-29 15:05:11 -0300

    template method pattern

---
+++ pattern/template_method/relatorio.rb
@@ -0,0 +1,30 @@
+class Relatorio
+  def imprime
+    imprime_cabecalho
+    imprime_conteudo
+  end
+end
+
+class HTMLRelatorio < Relatorio
+  def imprime_cabecalho
+    puts "<html>"
+  end
+  
+  def imprime_conteudo
+    puts "Dados do relatorio"
+  end
+end
+
+class TextoRelatorio < Relatorio
+  def imprime_cabecalho
+    puts "***"
+  end
+  
+  def imprime_conteudo
+    puts "Dados do relatorio"
+  end
+end
+
+HTMLRelatorio.new.imprime
+
+TextoRelatorio.new.imprime
